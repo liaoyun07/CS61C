25b85caf8a35c62f511154c3f4e6d2809694d842
liaoyun07 <liaoyun07@Outlook.com>
2023-09-06 13:09:20 +0800

    lab01

---
+++ lab01/ll_cycle.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include "ll_cycle.h"
+
+int ll_has_cycle(node *head) {
+    /* your code here */
+    node* tortoise = head;
+    node* hare = head;
+    while(hare && hare->next){
+    hare = hare->next->next;        
+    tortoise = tortoise->next;
+    if(tortoise == hare) return 1;
+    }
+    return 0;
+}
